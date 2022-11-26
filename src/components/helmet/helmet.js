89e962f5d651cd8e6b7c0e71392a77adab490c7f
Tobi Kesuma <tobikesuma1999@gmail.com>
2022-11-27 04:29:10 +0800

    fixing helmet

--- src/components/helmet/helmet.js
+++ src/components/helmet/helmet.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Helmet = (props) => {
-    document.title = props.title + ' - Team M11 Esports Bali';
+    document.title = props.title + ' | Team M11 Esports Bali';
   return (
     <div className='w-100'>{props.children}</div>
   )
--- src/components/ui/Match.jsx
+++ src/components/ui/Match.jsx
@@ -11,11 +11,17 @@ import '../../style/match.css'
 
 const Match = () => {
   return (
-    <div className='live-match-teams'>
-        <figure className='match-team match-1 img-holder' style={{width: '406', height:'277'}}>
-            <img src={imageData.MainLogo} width={406} height={277} loading={lazy} alt='' className='w-100' />
-        </figure>
-    </div>
+    <Container>
+      <Row>
+        <Col lg='6' md='6'>
+        <div className='live-match-teams'>
+          <figure className='match-team match-1 img-holder' style={{width: '406', height:'277'}}>
+            <img src={imageData.MainLogo} width={406} height={277} alt='' className='w-100' />
+          </figure>
+        </div>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
