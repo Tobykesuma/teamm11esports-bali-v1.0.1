acc186e21be7ec045daceba8f4b82eb4ff13a670
Tobi Kesuma <tobikesuma1999@gmail.com>
2022-11-29 06:44:11 +0800

    Create TournamentData.js

---
+++ src/data/TournamentData.js
@@ -0,0 +1,15 @@
+// Logo Tournament Achievements//
+import Tournament01 from '../assets/2022/tournament/esl.png'
+import Tournament02 from '../assets/2022/tournament/Intellogo.png'
+import Tournament03 from '../assets/2022/tournament/Lupon.png'
+import Tournament04 from '../assets/2022/tournament/Oxtradelogo.png'
+import Tournament05 from '../assets/2022/tournament/prasmul.png'
+
+
+export default {
+    Tournament01,
+    Tournament02,
+    Tournament03,
+    Tournament04,
+    Tournament05,
+}
