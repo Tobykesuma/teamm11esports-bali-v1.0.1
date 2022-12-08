2a6dc55e58ea7a0db6cd1f6a5fbe3ae398386217
Tobi Kesuma <tobikesuma1999@gmail.com>
2022-12-08 20:13:15 +0800

    update

--- src/data/imageData.js
+++ src/data/imageData.js
@@ -1,14 +1,14 @@
 // MainLogo //
-import MainLogo from '../assets/2022/images/mainlogo.png'
+import MainLogo from '../assets/2022/images/mainlogo.webp'
 import LiveMatchBanner from '../assets/2022/images/live-match-banner.jpg'
 import BlogImages from '../assets/2022/blogs/blog-1.jpg'
 import ShopBG from '../assets/2022/images/shop-bg.jpeg'
 
 // Background Games //
-import Dota2Bg from '../assets/2022/images/dota.jpeg'
-import PUBGMBg from '../assets/2022/images/pubg.jpeg'
-import MLBBBg from '../assets/2022/images/mobile-legend-bg.jpeg'
-import ValorantBg from '../assets/2022/images/valorant-bg.jpeg'
+import Dota2Bg from '../assets/2022/images/dota.webp'
+import PUBGMBg from '../assets/2022/images/pubg.webp'
+import MLBBBg from '../assets/2022/images/mobile-legend-bg.webp'
+import ValorantBg from '../assets/2022/images/valorant-bg.webp'
 
 
 
@@ -18,15 +18,15 @@ import Dota2Logo from '../assets/2022/images/logodota-2.png'
 
 // Live Match Image //
 import EnemyLogo from '../assets/2022/images/enemy-logo.jpg'
-import MainLogoBlack from '../assets/2022/images/logo-list-black.png';
+import MainLogoBlack from '../assets/2022/images/logo-list-black.webp';
 
 // Talent Image //
 import Talent01 from '../assets/2022/talent/team-member-1.png';
 
 // News Image //
-import NewsImage01 from '../assets/2022/blogs/news01.jpeg'
-import NewsImage02 from '../assets/2022/blogs/new02.jpeg'
-import NewsImage03 from '../assets/2022/blogs/new03.jpeg'
+import NewsImage01 from '../assets/2022/blogs/news01.webp'
+import NewsImage02 from '../assets/2022/blogs/new02.webp'
+import NewsImage03 from '../assets/2022/blogs/new03.webp'
 
 export default  {
     MainLogo,
