ea75fa9397681da7ca4071f6390197509a1c0a09
Tobi Kesuma <tobikesuma1999@gmail.com>
2022-12-12 11:35:20 +0800

    1

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
     setLoading(true)
     setTimeout(() => {
       setLoading(false)
-    }, 5000)
+    }, 2000)
   }, [])
 
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { CookiesProvider } from 'react-cookie';
+import * as serviceWorker from './serviceWorker';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +18,8 @@ root.render(
     </CookiesProvider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
