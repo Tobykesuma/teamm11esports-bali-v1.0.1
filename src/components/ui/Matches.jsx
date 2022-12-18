38c808add6873b9dbbad56e493fb5504bd4603e7
Tobi Kesuma <tobikesuma1999@gmail.com>
2022-12-18 18:18:04 +0800

    update

--- src/components/ui/Matches.jsx
+++ src/components/ui/Matches.jsx
@@ -4,6 +4,8 @@ import { Container, Row, Col } from 'react-bootstrap'
 import {AiFillYoutube} from 'react-icons/ai'
 import {FiTwitch} from 'react-icons/fi'
 import MatchesLogo from '../../data/MatchesLogoTeam'
+import { Link } from 'react-router-dom'
+
 
 const Matches = () => {
   return (
@@ -21,7 +23,7 @@ const Matches = () => {
                         <div className='match-status'>Upcoming</div>
                         <div className='match-tournament'>
                             <a href='https://www.gosugamers.net/dota2/tournaments/55051-esea-league'>
-                            <img src={MatchesLogo.DefaultLogoDota} alt='' />
+                            <img src={MatchesLogo.ESLLeagueLogo} alt='' />
                             ESEA LEAGUE
                             </a>
                         </div>
@@ -82,7 +84,7 @@ const Matches = () => {
                         <div className='match-status'>Upcoming</div>
                         <div className='match-tournament'>
                             <a href='https://www.gosugamers.net/dota2/tournaments/55051-esea-league'>
-                            <img src={MatchesLogo.DefaultLogoDota} alt='' />
+                            <img src={MatchesLogo.ESLLeagueLogo} alt='' />
                             ESEA LEAGUE
                             </a>
                         </div>
@@ -137,13 +139,75 @@ const Matches = () => {
                             </button>
                         </div>
                     </div>
-                
-                
+                    
+                    <div className='match'>
+                    <div className='match-header'>
+                        <div className='match-status'>Upcoming</div>
+                        <div className='match-tournament'>
+                            <a href='https://www.youtube.com/@wslofficial'>
+                            <img src={MatchesLogo.WSLLogo} alt='' />
+                            WSL SEASON 6 
+                            </a>
+                        </div>
+                        <div className='match-actions'>
+                            <button className='match-icons'>
+                                <a href='https://www.youtube.com/@wslofficial/streams'>
+                                    <AiFillYoutube />
+                                </a>
+                            </button>
+                            <button className='match-icons'>
+                                <a href='/'>
+                                    <FiTwitch />
+                                </a>
+                            </button>
+                        </div>
+                        </div>
+
+                        <div className='match-content'>
+                            <div className='match-column'>
+                                <div className='matches-team team--home'>
+                                    <div className='match-logo-team'>
+                                        <img src={MatchesLogo.LadiesHomeLogo} alt='' />
+                                    </div>
+                                    <h2 className='team-name'>Team M11</h2>
+                                </div>
+                            </div>
+                            <div className='match-column'>
+                                <div className='match-details'>
+                                    <div className='match-date'>
+                                        18 DECEMBER 2022 <br></br> 
+                                        <strong>12:20 WIB</strong>
+                                    </div>
+                                    <div className='match-score'>
+                                        <span className='match-score-number'> 0 </span>
+                                        <span className='match-score-divide'> : </span>
+                                        <span className='match-score-number'> 2 </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='match-column'>
+                                <div className='matches-team team--away'>
+                                    <div className='match-logo-team'>
+                                        <img src={MatchesLogo.DefaultLogoMLBB} alt='' />
+                                    </div>
+                                    <h2 className='team-name'>FOES WIN</h2>
+                                </div>
+                            </div>
+                            <button className='match-live-streams'>
+                                <a href='https://www.youtube.com/@wslofficial/streams' target='__blank'>
+                                    <AiFillYoutube />
+                                </a>
+                            </button>
+                        </div>
+                    </div>
 
 
-                    {/* <button className='all-schedule'>
-                    VIEW ALL SCHEDULE
-                </button> */}
+                    <button className='all-schedule-btn btn-primary'>
+                        <Link to='/schedule'>
+                            VIEW ALL SCHEDULE
+                        </Link>
+                </button>
+                
             </Col>
         </Row>
     </Container>
--- src/data/MatchesLogoTeam.js
+++ src/data/MatchesLogoTeam.js
@@ -1,23 +1,30 @@
 // Main Logo //
 import HomeLogo from '../assets/2022/matches/logo-list-black.webp'
-
+import LadiesHomeLogo from '../assets/2022/matches/M11-Ladies-Logo.webp'
 
 // Logo Dota + Enemy No Logo //
 import DefaultLogoDota from '../assets/2022/matches/logodota-2.webp'
 
+// Logo MLBB + Enemy No Logo //
+import DefaultLogoMLBB from '../assets/2022/matches/mobile-legend-logo.webp'
 
 // Enemy Logo //
 import ReaperLogo from '../assets/2022/matches/reaper.png'
 
 
 // Tournament Logo Ongoing //
-
-
+import WSLLogo from '../assets/2022/matches/WSL-Logo.png'
+import ESLLeagueLogo from '../assets/2022/matches/ESEA-LEAGUE.png'
 
 const MatchesLogo = {
     HomeLogo,
     DefaultLogoDota,
-    ReaperLogo
+    DefaultLogoMLBB,
+    ReaperLogo,
+    LadiesHomeLogo,
+    WSLLogo,
+    ESLLeagueLogo
+
 
 }
 
